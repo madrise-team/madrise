68b679a2b11ecef4bc23cfc0585331521a8d1758
CrazyComrad <crazycomrad@gmail.com>
2023-01-24 21:06:20 +0400

    GOD AFK
    божеский вид афк

--- resources/reglog/AFK.lua
+++ resources/reglog/AFK.lua
@@ -1,43 +1,60 @@
 
 local afk = false
 local def = false
-local bool = false
-local mas = {}
+local remember = {}
 
-function time(bool)
+function PlyPos1 ()
 	local x,y,z = getElementPosition(localPlayer)
-	local sum = x + y + z
-	mas[1] = sum
+	local sum1 = x + y + z
+	remember[1] = sum1
 end
 
-function ff ()
+function PlyPos2 ()
 	local x1,y1,z1 = getElementPosition(localPlayer)
-	local sum1 = mas[1]
 	local sum2 = x1 + y1 + z1
-	if sum2 == sum1 then
+	return sum2
+end	
+
+function Check1 ()
+	local plyPos1 = remember[1]
+	local plyPos2 = PlyPos2()
+	if plyPos1 == plyPos2 then
 		afk = true
 	else
 		print("я живой")
 	end	
 	return afk
-end	
+end
 
-function oo ()
-	local def = ff()	
+function Check ()
+	local def = Check1()	
 	if def then 
 		print("ты тут?")
 		afk = false
 		def = false
-		mas[1] = nil
+		remember[1] = nil
 	end	
 end
 
 function timer()
-	setTimer(time,3000,1)
-	setTimer(oo,5000,1)
+	setTimer(PlyPos1,3000,1)
+	setTimer(Check,5000,1)
 end
 
 setTimer(timer, 6000, 0)	
+
+--[[
+Происходит проверка шевеления игрока каждые 11 секунд 
+Если игрок стоит в течении 11 секунд то ему пишется что он стоит 
+если он шевелится во время проверки то пишется что он живой и проверка обновляется 
+ТРЕБУЕТСЯ ОТЛАЖИВАНИЕ И НАСТРОЙКА А ЕЩЕ КОД ГРЯЗНЫЙ ШО ПИЗДЕЦ!!!!
+--]]
+
+
+
+
+
+
 
 --[[
 Есть точка где встал плеер вокруг него очерчивается круг радиусом 5 метров 
