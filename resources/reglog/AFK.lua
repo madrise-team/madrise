b4c2be17172057145cf3ceefd65da365effb7974
CrazyComrad <crazycomrad@gmail.com>
2023-01-26 04:36:22 +0400

    AFK 2.0
    почти готовая система

--- resources/reglog/AFK.lua
+++ resources/reglog/AFK.lua
@@ -1,64 +1,46 @@
-
-local afk = false
-local def = false
-local remember = {}
+local XC,YC,ZC
+local radius = 10
 
 function PlyPos1 ()
-	local x,y,z = getElementPosition(localPlayer)
-	local sum1 = x + y + z
-	remember[1] = sum1
+	local x,y,z = getElementPosition(localPlayer) -- Позиция игрока до начала проверки нахождения его в радиусе афк
+	XC,YC,ZC = x,y,z
 end
 
 function PlyPos2 ()
-	local x1,y1,z1 = getElementPosition(localPlayer)
-	local sum2 = x1 + y1 + z1
-	return sum2
+	local x1,y1,z1 = getElementPosition(localPlayer) -- Позиция игрока в момент проверки
+	return x1,y1,z1
 end	
 
 function Check1 ()
-	local plyPos1 = remember[1]
-	local plyPos2 = PlyPos2()
-	if plyPos1 == plyPos2 then
+	local xc,yc,zc = XC,YC,ZC --центр окружности 
+	local xn,yn,zn = PlyPos2()
+	local dst = getDistanceBetweenPoints3D(xc,yc,zc,xn,yn,zn)
+	if dst < radius then
 		afk = true
 	else
-		print("я живой")
+		afk = false
 	end	
 	return afk
 end
 
 function Check ()
-	local def = Check1()	
-	if def then 
-		print("ты тут?")
-		afk = false
-		def = false
-		remember[1] = nil
+	local afk = Check1()	
+	if afk then 
+		print("ты тут?") -- Меняется на нужные компоненты 
+	else
+		print("я живой") -- Меняется на нужные компоненты
+		PlyPos1()
 	end	
 end
 
-function timer()
-	setTimer(PlyPos1,3000,1)
-	setTimer(Check,5000,1)
-end
-
-setTimer(timer, 6000, 0)	
+PlyPos1()
+setTimer(Check,10000,0)
 
 --[[
-Происходит проверка шевеления игрока каждые 11 секунд 
-Если игрок стоит в течении 11 секунд то ему пишется что он стоит 
-если он шевелится во время проверки то пишется что он живой и проверка обновляется 
-ТРЕБУЕТСЯ ОТЛАЖИВАНИЕ И НАСТРОЙКА А ЕЩЕ КОД ГРЯЗНЫЙ ШО ПИЗДЕЦ!!!!
+скелет готов осталось накинуть конечный функционал и добавит фронты 
+так же отрегулировать время проверки и расстояние
 --]]
-
-
-
-
-
-
-
 --[[
-Есть точка где встал плеер вокруг него очерчивается круг радиусом 5 метров 
-проводится проверка положения плеера если плеер находится внутри куруга ? минут 
-то ему приходит сообщение что он гей он игнорит его кикает или если плеер съебывает из круга 
-то круг обновляется система хуйня но сделать надо
+пробовал сделать переключалку чтобы не происходило проверок если плеер двигается но видимо похуй на 
+оперативку игроков
 --]]
