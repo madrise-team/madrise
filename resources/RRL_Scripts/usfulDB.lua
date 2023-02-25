cd810536aa17f8c1516330914be441b9214af237
MihalkaProMuzon <NeOchHoroho2001@mail.ru>
2023-02-25 18:35:49 +0400

    DB mini optimize
    Небольшая реорганизация и оптимизация скрипта usfulDB

--- resources/RRL_Scripts/usfulDB.lua
+++ resources/RRL_Scripts/usfulDB.lua
@@ -12,6 +12,11 @@ function stringFromArayDbFormatt(array)
     return str
 end
 
+--////////////////////////////////////////////////////////////////////////////////////////////////////
+--  classic data 
+--////////////////////////////////////////////////////////////////////////////////////////////////////
+
+-- return {1:{row}, 2:{row}}
 function getDbDataFromArray(aTable,serachColumn,valuesArray,callback)			--- Поиск по массиву совпадений (Where in array)
 	local qh
 	qh = dbQuery(function()
@@ -20,30 +25,39 @@ function getDbDataFromArray(aTable,serachColumn,valuesArray,callback)			--- По
 
 end
 
+-- return {1:{column}, 2:{column}}
 function getDbColumnData(aTable,serachColumn,serachColumnValue,column,callback) -- 1 столбец вместо всех строки
 	local qh
 	qh = dbQuery(function()
 		if type(callback)=='function' then callback(dbPoll(qh,0)) end
 	end,SQLStorage,"SELECT `??` FROM `??` WHERE (`??` = ?)",column,aTable,serachColumn,serachColumnValue)
 end
+
+-- return {column}
 function get1DbColumnData(aTable,serachColumn,serachColumnValue,column,callback)
 	getDbColumnData(aTable,serachColumn,serachColumnValue,column,function(data)
 		data = data or {}
 		callback(data[1])
 	end)
 end
+
+-- return {1:{row}, 2:{row}}
 function getDbData(aTable,serachColumn,value,callback) -- вся строка всех встрок
 	local qh
 	qh = dbQuery(function()
 		if type(callback)=='function' then callback(dbPoll(qh,0)) end
 	end,SQLStorage,"SELECT * FROM `??` WHERE (`??` = ?)",aTable,serachColumn,value)
 end
+
+-- return {row}
 function get1DbData(aTable,serachColumn,value,callback)
 	getDbData(aTable,serachColumn,value,function(data)
 		data = data or {}
 		callback(data[1])
 	end)
 end
+
+-- return {row1,row2,row3}
 function getDbRows(aTable,callback) -- все строки таблицы
 	local qh
 	qh = dbQuery(function()
@@ -52,26 +66,35 @@ function getDbRows(aTable,callback) -- все строки таблицы
 end
 
 
+--////////////////////////////////////////////////////////////////////////////////////////////////////
+--  Limited data 
+--////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-function getLimitedDbColumnRows(aTable,column,rowFrom,rowCount,callback) -- все столбцы строк от до
+-- return { rowFrom:{column} -- (rowFrom+rowCount):{column} }
+function getLimitedDbColumnRows(aTable,column,rowFrom,rowCount,callback) 
 	local qh
 	qh = dbQuery(function()
 		if type(callback)=='function' then callback(dbPoll(qh,0)) end
 	end,SQLStorage,"SELECT `??` FROM `??` LIMIT ?, ?",column,aTable,rowFrom,rowCount)
 end
+
+-- return { rowFrom:{row} -- (rowFrom+rowCount):{row} }
 function getLimitedDbRows(aTable,rowFrom,rowCount,callback) -- все строки от до
 	local qh
 	qh = dbQuery(function()
 		if type(callback)=='function' then callback(dbPoll(qh,0)) end
 	end,SQLStorage,"SELECT * FROM `??` LIMIT ?, ?",aTable,rowFrom,rowCount)
 end
+
+-- return { rowFrom:{column} -- (rowFrom+rowCount):{column} }
 function getLimitedDbColumnData(aTable,column,serachColumn,value,rowFrom,rowCount,callback) -- столбцы строк от до с условием
 	local qh
 	qh = dbQuery(function()
 		if type(callback)=='function' then callback(dbPoll(qh,0)) end
 	end,SQLStorage,"SELECT `??` FROM `??` WHERE (`??` = ?) LIMIT ?, ?",column,aTable,serachColumn,value,rowFrom,rowCount)
 end
+
+-- return { rowFrom:{row} -- (rowFrom+rowCount):{row} }
 function getLimitedDbData(aTable,serachColumn,value,rowFrom,rowCount,callback) -- строки от до
 	local qh
 	qh = dbQuery(function()
@@ -80,13 +103,12 @@ function getLimitedDbData(aTable,serachColumn,value,rowFrom,rowCount,callback) -
 end
 
 
-
-
-
 function setDbColumnValueByColumnSearch(aTable,serachColumn,serachColumnValue,updateColumn,updateColumnValue)
 	dbExec(SQLStorage,"UPDATE `??` SET `??` = ? WHERE `??` = ?",aTable,updateColumn,updateColumnValue,serachColumn,serachColumnValue)
 end
 
+
+-- return rowsCount
 function getTableRowsCount(aTable,callback)
 	local qh
 	qh = dbQuery(function()
@@ -96,12 +118,16 @@ end
 
 
 
-----------------------------------------------------------------------------------Array clumns -------------------
+--////////////////////////////////////////////////////////////////////////////////////////////////////
+--  Array clumns 
+--////////////////////////////////////////////////////////////////////////////////////////////////////
+
 function elmColumnArrayOperation(aTable,serachColumn,serachColumnValue,updateColumn,operation)
-	get1DbData(aTable,serachColumn,serachColumnValue,function(data)
+	get1DbColumnData(aTable,serachColumn,serachColumnValue,updateColumn,function(data)
 		local theTable
 		local colval = data[updateColumn]
 		if colval then theTable = tonumberIdnx(fromJSON(colval)) end
+		if type(theTable) ~= "table" then theTable = {} end
 		theTable = operation(theTable)
 
 		local theTableString = toJSON(theTable,true)
@@ -110,30 +136,24 @@ function elmColumnArrayOperation(aTable,serachColumn,serachColumnValue,updateCol
 end
 function addElmToColumnArray(aTable,serachColumn,serachColumnValue,updateColumn,arrayElmKey,arrayElmValue)
 	elmColumnArrayOperation(aTable,serachColumn,serachColumnValue,updateColumn,function(theTable)
-		if type(theTable) ~= "table" then theTable = {} end
 		theTable[arrayElmKey] = arrayElmValue
 		return theTable
 	end)
 end
 function removeElmFromColumnArray(aTable,serachColumn,serachColumnValue,updateColumn,arrayElmKey)
 	elmColumnArrayOperation(aTable,serachColumn,serachColumnValue,updateColumn,function(theTable)
-		if type(theTable) ~= "table" then return end
 		theTable[arrayElmKey] = nil
 		return theTable
 	end)
 end
 function addElmByValueToColumnArray(aTable,serachColumn,serachColumnValue,updateColumn,arrayElmValue)
 	elmColumnArrayOperation(aTable,serachColumn,serachColumnValue,updateColumn,function(theTable)
-		if type(theTable) ~= "table" then theTable = {} end
-
 		theTable[#theTable + 1] = arrayElmValue
-
 		return theTable
 	end)
 end
 function removeElmByValueFromColumnArray(aTable,serachColumn,serachColumnValue,updateColumn,arrayElmValue)
 	elmColumnArrayOperation(aTable,serachColumn,serachColumnValue,updateColumn,function(theTable)
-		if type(theTable) ~= "table" then return end
 		for k,v in pairs(theTable) do
 			if v == arrayElmValue then
 				theTable[k] = nil
